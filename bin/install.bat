9dd5f6ab5ea6c0ac379e893dabdf9d634515816c
Ryan Frame <code@ryanjframe.com>
2019-11-14 13:28:40 -0500

    Add Windows install script.

---
+++ bin/install.bat
@@ -0,0 +1,4 @@
+mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\src\dot-files\.vimrc"
+mklink /J "%USERPROFILE%\.vim" "%USERPROFILE%\src\dot-files\.vim"
+
+mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\src\dot-files\.gitconfig"
