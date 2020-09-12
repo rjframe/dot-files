ffb9ea3563a2219f4de83eb79ebecbc0f1d69377
Ryan Frame <code@ryanjframe.com>
2020-09-12 10:38:06 -0400

    Add kitty config.

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,7 @@
 # Run this from the dot-files directory.
 # TODO: Both install scripts should source a common one to remove duplication.
 
-sudo pacman -S rsync openssh gawk vim w3m zsh
+sudo pacman -S rsync openssh gawk vim w3m zsh ttf-fira-code
 
 BASE=`pwd`
 
@@ -17,6 +17,7 @@ ln -sr "$BASE/.config/shells" "$HOME/.config/shells"
 
 ln -sr "$BASE/.config/sway" "$HOME/.config/sway"
 ln -sr "$BASE/.config/i3status" "$HOME/.config/i3status"
+ln -sr "$BASE/.config/kitty" "$HOME/.config/kitty"
 
 ln -sr "$BASE/.vim" "$HOME/.vim"
 ln -s "$BASE/.vimrc" "$HOME/.vimrc"
